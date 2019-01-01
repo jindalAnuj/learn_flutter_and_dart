fe2d5f4b74f837053b88270c65f1a70b0cd7e1b6
Anuj Jndal <anujjindal121@yahoo.com>
2019-01-01 18:35:01 +0530

    basic list with column

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,20 @@
 import 'package:flutter/material.dart';
 
+import './product_manager.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('EasyList'),
         ),
-        home: new Scaffold(
-          appBar: AppBar(
-
-            title: Text('Learn Dart And Flutter'),
-            titleSpacing: 40,
-
-          ),
-          body: Text('Flutter Demo Home Page'),
-        ));
+        body: ProductManager(),
+      ),
+    );
   }
 }
